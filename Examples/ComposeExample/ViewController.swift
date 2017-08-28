e278389a7e181e26e6f44365228eceac5afd0722
Luke Zhao <me@lkzhao.com>
2017-08-28 11:34:38 -0400

    revert example

--- Examples/ComposeExample/ViewController.swift
+++ Examples/ComposeExample/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CollectionKit
 
 let bodyInset = UIEdgeInsets(top: 10, left: 16, bottom: 10, right: 16)
-let headerInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+let headerInset = UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16)
 
 func section(title: String, subtitle: String? = nil) -> AnyCollectionProvider {
   let titleSection = LabelCollectionProvider(text: title, font: .boldSystemFont(ofSize: 30), insets: headerInset)
@@ -25,6 +25,7 @@ func section(title: String, subtitle: String? = nil) -> AnyCollectionProvider {
 func space(_ height: CGFloat) -> AnyCollectionProvider {
   return SpaceCollectionProvider(sizeStrategy: (.fill, .absolute(height)))
 }
+
 
 let presenterSection: AnyCollectionProvider = {
   let presenters = [
@@ -76,10 +77,9 @@ let presenterSection: AnyCollectionProvider = {
   return CollectionComposer(
     layout: FlexLayout(flex: ["providerContent": FlexValue(flex: 1)]),
     section(title: "Presenter",
-            subtitle: "Presenter can be used customize the presentation of the child views. It is independent of the layout and have direct access to the view object. It is the perfect place to add animations."),
+            subtitle: "Presenter can be used customize the presentation of the child views. It is independent of the layout and have direct access to the view object. It is the perfect place to add animations to an existing provider."),
     buttonsCollectionViewProvider,
-    providerCollectionViewProvider,
-    section(title: "Testing")
+    providerCollectionViewProvider
   )
 }()
 
@@ -114,24 +114,50 @@ class ViewController: UIViewController {
       sizeProvider: imageSizeProvider,
       presenter: WobblePresenter()
     )
-
     
-//    collectionView.provider = CollectionComposer(
-//      space(100),
-//      section(title: "CollectionKit", subtitle: "A modern swift framework for building reusable collection view components."),
-//      space(20),
-//      section(title: "Horizontal Waterfall Layout"),
-//      image1Section,
-//      space(20),
-//      section(title: "Vertical Waterfall Layout"),
-//      image2Section,
-//      space(20),
-//      presenterSection
-//    )
-
+    let coreConceptSection: AnyCollectionProvider = {
+      let concepts: [(String, String)] = [
+        ("Data Provider", "The data source for the collection. It provides a homogenious set of data. The data will be passed to view provider, layout provider, and responder."),
+        ("View Provider", "Provides corresponding UIView object for each of the data provided by the data source."),
+        ("Layout Provider", "Provides layout information for the collection view."),
+        ("Size Provider", "Provides size information to the layout provider."),
+        ("Responder", "Provides event handler to the collection view events like tap, drag, & reload."),
+        ("Presenter", "Can be used to customize the presentation of the child views. It is independent of the layout and have direct access to the view object. It is the perfect place to add animations to an existing provider.")
+      ]
+      let collectionView = CollectionView()
+      collectionView.clipsToBounds = false
+      collectionView.provider = CollectionProvider(
+        data: concepts,
+        viewUpdater: { (view: CardView, data: (String, String), at: Int) in
+          view.title = data.0
+          view.subtitle = data.1
+        },
+        layout: WaterfallLayout(columns:1, insets: bodyInset, axis: .horizontal),
+        sizeProvider: { (_, _, size) -> CGSize in
+          return size
+        },
+        tapHandler: { [weak self] _, index, dataProvider in
+          self?.present(MessagesViewController(), animated: true, completion: nil)
+        }
+      )
+      
+      return ViewCollectionProvider(collectionView, sizeStrategy: (.fill, .absolute(200)))
+    }()
+    
     collectionView.provider = CollectionComposer(
-      layout: FlexLayout(padding: 10, justifyContent: .spaceAround, alignItems: .center),
-      (0...3).map{ section(title: "Section \($0)") }
+      space(100),
+      section(title: "CollectionKit", subtitle: "A modern swift framework for building reusable collection view components."),
+      space(20),
+      section(title: "Core Concepts"),
+      coreConceptSection,
+      space(20),
+      section(title: "Horizontal Waterfall Layout"),
+      image1Section,
+      space(20),
+      section(title: "Vertical Waterfall Layout"),
+      image2Section,
+      space(20),
+      presenterSection
     )
   }
   
