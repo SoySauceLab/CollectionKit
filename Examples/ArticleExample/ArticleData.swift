906f75db6abd3846c452378cb4aa763ad7aacedc
Yansong Li <liyansong.edw@gmail.com>
2017-09-04 14:58:05 -0400

    Add in MediumViewController and related objects. (#20)
    * Add in ArticleExampleViewController and make a group for it.

---
+++ Examples/ArticleExample/ArticleData.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleData.swift
+//  CollectionKit
+//
+//  Created by yansong li on 2017-09-03.
+//  Copyright © 2017 lkzhao. All rights reserved.
+//
+
+import UIKit
+
+struct ArticleData {
+  let hueValue: CGFloat
+  let title: String
+  let subTitle: String
+}
