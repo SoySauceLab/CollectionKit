577ba1220d7afa44208c71b6971423c8ccfa3e96
Luke Zhao <me@lkzhao.com>
2017-11-30 13:50:09 -0500

    add sizeStrategyOverride

--- Sources/Addon/ViewCollectionProvider.swift
+++ Sources/Addon/ViewCollectionProvider.swift
@@ -9,16 +9,32 @@
 import UIKit
 
 open class ViewCollectionProvider: CollectionProvider<UIView, UIView> {
-  private class ViewProvider: CollectionViewProvider<UIView, UIView> {
-    override func view(data: UIView, index: Int) -> UIView {
-      return data
-    }
-  }
 
   public enum ViewSizeStrategy {
     case fill
     case fit
     case absolute(CGFloat)
+  }
+
+  var arrayDataProvider: ArrayDataProvider<UIView> {
+    return dataProvider as! ArrayDataProvider<UIView>
+  }
+
+  var views: [UIView] {
+    get { return arrayDataProvider.data }
+    set {
+      guard arrayDataProvider.data != newValue else { return }
+      arrayDataProvider.data = newValue
+    }
+  }
+
+  public var sizeStrategy: (width: ViewSizeStrategy, height: ViewSizeStrategy)
+  public var sizeStrategyOverride: [UIView: (width: ViewSizeStrategy, height: ViewSizeStrategy)] = [:]
+
+  private class ViewProvider: CollectionViewProvider<UIView, UIView> {
+    override func view(data: UIView, index: Int) -> UIView {
+      return data
+    }
   }
 
   public convenience init(identifier: String? = nil,
@@ -31,23 +47,26 @@ open class ViewCollectionProvider: CollectionProvider<UIView, UIView> {
 
   public init(identifier: String? = nil,
               views: [UIView],
-              sizeStrategy: (ViewSizeStrategy, ViewSizeStrategy) = (.fit, .fit),
+              sizeStrategy: (width: ViewSizeStrategy, height: ViewSizeStrategy) = (.fit, .fit),
               layout: CollectionLayout<UIView>) {
+    self.sizeStrategy = sizeStrategy
     super.init(dataProvider: ArrayDataProvider(data: views, identifierMapper: {
                  return "\($1.hash)"
                }),
                viewProvider: ViewProvider())
     self.identifier = identifier
     self.layout = layout
-    sizeProvider = { (_, view, size) -> CGSize in
+    sizeProvider = { [unowned self] (_, view, size) -> CGSize in
       let fitSize = view.sizeThatFits(size)
+      let sizeStrategy = self.sizeStrategyOverride[view] ?? self.sizeStrategy
       let width: CGFloat, height: CGFloat
-      switch sizeStrategy.0 {
+
+      switch sizeStrategy.width {
       case .fit: width = fitSize.width
       case .fill: width = size.width
       case .absolute(let value): width = value
       }
-      switch sizeStrategy.1 {
+      switch sizeStrategy.height {
       case .fit: height = fitSize.height
       case .fill: height = size.height
       case .absolute(let value): height = value
