a7b9bd86b6903b93fb6f223afd1808639ad0c1d1
Hiroshi Horie <private@hiroshihorie.com>
2017-12-25 00:09:46 +0700

    use NSStringFromClass

--- Sources/Other/CollectionReuseViewManager.swift
+++ Sources/Other/CollectionReuseViewManager.swift
@@ -18,7 +18,7 @@ public class CollectionReuseViewManager: NSObject {
   public var lifeSpan: TimeInterval = 0.5
 
   public func queue(view: UIView) {
-    let identifier = String(describing: type(of: view))
+    let identifier = NSStringFromClass(type(of: view))
     view.reuseManager = nil
     if reusableViews[identifier] != nil && !reusableViews[identifier]!.contains(view) {
       reusableViews[identifier]?.append(view)
@@ -31,7 +31,8 @@ public class CollectionReuseViewManager: NSObject {
   }
 
   public func dequeue<T: UIView> (_ defaultView: @autoclosure () -> T) -> T {
-    let queuedView = reusableViews[String(describing: T.self)]?.popLast() as? T
+    let identifier = NSStringFromClass(T.self)
+    let queuedView = reusableViews[identifier]?.popLast() as? T
     let view = queuedView ?? defaultView()
     if let view = view as? CollectionViewReusableView {
       view.prepareForReuse()
@@ -41,7 +42,8 @@ public class CollectionReuseViewManager: NSObject {
   }
 
   public func dequeue<T: UIView> (type: T.Type) -> T {
-    let queuedView = reusableViews[String(describing: type)]?.popLast() as? T
+    let identifier = NSStringFromClass(type.self)
+    let queuedView = reusableViews[identifier]?.popLast() as? T
     let view = queuedView ?? type.init()
     if let view = view as? CollectionViewReusableView {
         view.prepareForReuse()
