1fe6917a0ccd0e17ecbd1a42417ebfcfed860e40
jindulys <liyansong.edw@gmail.com>
2018-02-19 12:37:29 -0500

    Add in CollectionViewCollectionProvider.

---
+++ Sources/Addon/CollectionViewCollectionProvider.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewProvider.swift
+//  CollectionKit
+//
+//  Created by yansong li on 2018-02-18.
+//  Copyright © 2018 lkzhao. All rights reserved.
+//
+
+import UIKit
+
+open class CollectionViewProvider: ViewCollectionProvider {
+
+}
