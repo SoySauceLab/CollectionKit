37f496bcb5afcfbdd523895c7e3229bf15107eeb
zjade <zjade@outlook.com>
2019-08-12 19:18:23 +0800

    swiftlint format

--- Sources/SizeSource/AutoLayoutSizeSource.swift
+++ Sources/SizeSource/AutoLayoutSizeSource.swift
@@ -7,22 +7,22 @@ open class AutoLayoutSizeSource<Data, View: UIView>: SizeSource<Data> {
   private let dummyViewUpdater: ClosureViewUpdateFn<Data, View>
   private let horizontalFittingPriority: UILayoutPriority
   private let verticalFittingPriority: UILayoutPriority
-   public init(dummyView: View.Type, 
-              horizontalFittingPriority: UILayoutPriority = .defaultHigh, 
-              verticalFittingPriority: UILayoutPriority = .defaultLow, 
+  public init(dummyView: View.Type,
+              horizontalFittingPriority: UILayoutPriority = .defaultHigh,
+              verticalFittingPriority: UILayoutPriority = .defaultLow,
               viewUpdater: @escaping ClosureViewUpdateFn<Data, View>) {
-    
+
     self.dummyView = View()
     self.dummyViewUpdater = viewUpdater
     self.horizontalFittingPriority = horizontalFittingPriority
     self.verticalFittingPriority = verticalFittingPriority
   }
-   open override func size(at index: Int, data: Data, collectionSize: CGSize) -> CGSize {
+  open override func size(at index: Int, data: Data, collectionSize: CGSize) -> CGSize {
     self.dummyViewUpdater(self.dummyView, data, index)
-    
+
     return self.dummyView.systemLayoutSizeFitting(
-      collectionSize, 
-      withHorizontalFittingPriority: self.horizontalFittingPriority, 
-      verticalFittingPriority: self.verticalFittingPriority)
+        collectionSize,
+        withHorizontalFittingPriority: self.horizontalFittingPriority,
+        verticalFittingPriority: self.verticalFittingPriority)
   }
 }
